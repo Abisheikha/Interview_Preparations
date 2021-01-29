ead944582b59932cb5c8244ad685f2cf293e85fc
Abisheikha <73419608+Abisheikha@users.noreply.github.com>
2021-01-29 12:54:58 +0530

    Update overlapping intervals.c

--- overlapping intervals.c
+++ overlapping intervals.c
@@ -1,8 +1,11 @@
+//You are given an array of intervals- that is, an array of tuples(start,end). The array may not be sorted, and could contain overlapping intervals.
+//Return another array where the overlapping intervals are merged. Input:[(1.3),(5,8),(4,10),(20,25)] Output:[(1,3),(4,10),(20,25)]
+
 #include<stdio.h>
 #include<stdlib.h>
 void main()
 {
-	int arr[50][10],i,j,n,temp1,temp2,l,k,b[50][50];
+	int arr[50][10],i,j,n,temp1,temp2,l,k;
 	printf("Enter the number of pairs:");
 	scanf("%d",&n);
 	for(i=0;i<n;i++)
